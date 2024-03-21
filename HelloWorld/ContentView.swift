bef8803fde25c787f33e5f9d5e472339155760b9
Alex <onischenko_ay@inbox.ru>
2024-03-22 02:33:52 +0300

    Initial Commit

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Alexander Onishchenko on 22.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Alexander Onishchenko on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
