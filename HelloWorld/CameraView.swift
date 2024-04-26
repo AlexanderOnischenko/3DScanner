ce93375b19f282f0994b439686c0627180494e5d
Alex <onischenko_ay@inbox.ru>
2024-04-26 13:05:20 +0300

    focus function added

--- HelloWorld/CameraView.swift
+++ HelloWorld/CameraView.swift
@@ -17,6 +17,7 @@ class CameraView: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
     private var previewLayer = AVCaptureVideoPreviewLayer()
     private var photoOutput: AVCapturePhotoOutput?
     var screenRect: CGRect! = nil // For view dimensions
+    private var focusIndicator: UIView!
     
     var webServer = GCDWebServer()
         
@@ -25,7 +26,9 @@ class CameraView: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
         
    //     sessionQueue.async { [unowned self] in
    //         guard permissionGranted else { return }
-           self.setupCaptureSession()
+        self.setupCaptureSession()
+        setupFocusIndicator()
+ 
             
    //         self.setupLayers()
    //         self.setupDetector()
@@ -34,7 +37,15 @@ class CameraView: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
             // Вызовите startRunning() на фоновом потоке
             self.captureSession.startRunning()
         }
+        // Updates to UI must be on main queue
+        DispatchQueue.main.async { [weak self] in
+            self!.setupLayers()
+        }
+
         setupWebServer()
+        // Добавляем Gesture Recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(focusAndExposeTap(_:)))
+        view.addGestureRecognizer(tapGesture)
     }
     
     func checkPermission() {
@@ -162,21 +173,67 @@ class CameraView: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
             guard captureSession.canAddInput(videoDeviceInput) else { return }
             captureSession.addInput(videoDeviceInput)
             captureSession.addOutput(photoOutput!)
-                             
-            // Preview layer
-            screenRect = UIScreen.main.bounds
-            
-            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
-            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // Fill screen
-            previewLayer.connection?.videoOrientation = .portrait
- 
-            // Updates to UI must be on main queue
-            DispatchQueue.main.async { [weak self] in
-                self!.view.layer.addSublayer(self!.previewLayer)
-            }
         }
     
+    @objc func focusAndExposeTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        let location = gestureRecognizer.location(in: view)
+        let devicePoint = previewLayer.captureDevicePointConverted(fromLayerPoint: location)
+        updateFocusIndicator(at: location)
+        focus(at: devicePoint)
+    }
+    
+    func focus(at point: CGPoint) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        do {
+            try device.lockForConfiguration()
+            if device.isFocusPointOfInterestSupported {
+                device.focusPointOfInterest = point
+                device.focusMode = .autoFocus
+            }
+            device.unlockForConfiguration()
+        } catch {
+            print("Не удалось заблокировать конфигурацию камеры для фокусировки")
+        }
+    }
+    
+    private func setupFocusIndicator() {
+        focusIndicator = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        focusIndicator.layer.borderColor = UIColor.yellow.cgColor
+        focusIndicator.layer.borderWidth = 2
+        focusIndicator.layer.cornerRadius = 40
+        focusIndicator.backgroundColor = .clear
+        focusIndicator.isHidden = true
+        view.addSubview(focusIndicator)
+    }
+    
+    private func updateFocusIndicator(at point: CGPoint) {
+        focusIndicator.center = point
+        focusIndicator.isHidden = false
+        UIView.animate(withDuration: 0.3, animations: {
+            self.focusIndicator.transform = CGAffineTransform(scaleX: 1.25, y: 1.25)
+        }) { _ in
+            UIView.animate(withDuration: 0.3) {
+                self.focusIndicator.transform = CGAffineTransform.identity
+            }
+        }
+    }
+    
+    private func setupPreviewLayer() {
+        // Preview layer
+        screenRect = UIScreen.main.bounds
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill // Fill screen
+        previewLayer.connection?.videoOrientation = .portrait
+        self.view.layer.addSublayer(previewLayer)
+    }
+    
+    
+    private func setupLayers() {
+        self.setupPreviewLayer()
+        self.setupFocusIndicator()  // Ensure focusIndicator is on top of the camera layer
+    }
     
     }
 
--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -12,73 +12,80 @@ import AVFoundation
 struct ContentView : View {
     @ObservedObject var myBT = BluetoothController()
     var body: some View {
-        ZStack {
-            CameraViewRepresentable()
-                            .edgesIgnoringSafeArea(.all)
-              VStack {
+         ZStack {
+             CameraViewRepresentable()
+                 .edgesIgnoringSafeArea(.all)
+             VStack {
+                 controlPanel
+                 Spacer()
+                 bottomButtons
+             }
+         }
+     }
 
-                      HStack {
-                          Button(action: myBT.increaseNumber) {
-                              Image(systemName: "arrow.up")
-                                  .font(.system(size: 24))
-                                  .foregroundColor(.white)
-                                  .padding()
-                                  .frame(height: 48) // Устанавливаем высоту кнопки по высоте текста
-                                  .background(Color.green)
-                                  .cornerRadius(5)
-                          }
-                          
-                          Text("\(myBT.getNumshots())")
-                              .font(.system(size: 48)) // Крупные цифры
-                              .foregroundColor(.white) // Белый цвет текста
-                              .padding() // Добавляем немного отступа вокруг текста
-                              .background(Color.blue) // Синий фон
-                              .cornerRadius(10) // Закругляем углы фона
-                              .padding()
-                          
-                          Button(action: myBT.decreaseNumber) {
-                              Image(systemName: "arrow.down")
-                                  .font(.system(size: 24))
-                                  .foregroundColor(.white)
-                                  .padding()
-                                  .frame(height: 48) // Устанавливаем высоту кнопки по высоте текста
-                                  .background(Color.red)
-                                  .cornerRadius(5)
-                          }
-                      }
-                      .padding()
-                  
-                  Spacer()
-                  HStack {
-                      Spacer()
-                      Button(action: {
-                          // Действие для кнопки Start
-                          myBT.startScanning()
-                          
-                      }) {
-                          Image(systemName: "play.circle") // Используйте подходящую SF Symbol
-                              .resizable()
-                              .frame(width: 50, height: 50)
-                      }
-                      
-                      Spacer()
-                      
-                      Button(action: {
-                          // Действие для кнопки Stop
-                          myBT.stopScanning()
-                      }) {
-                          Image(systemName: "stop.circle") // Используйте подходящую SF Symbol
-                              .resizable()
-                              .frame(width: 50, height: 50)
-                              .foregroundColor(.red) // Делает иконку красной
-                      }
-                      Spacer()
-                  }
-                  .padding()
-                  }
-              }
-          }
-      }
+     private var controlPanel: some View {
+         HStack {
+             Button(action: myBT.increaseNumber) {
+                 Image(systemName: "arrow.up")
+                     .font(.system(size: 24))
+                     .foregroundColor(.white)
+                     .padding()
+                     .frame(height: 48)
+                     .background(Color.green)
+                     .cornerRadius(5)
+             }
+             
+             Text("\(myBT.getNumshots())")
+                 .font(.system(size: 48))
+                 .foregroundColor(.white)
+                 .padding()
+                 .background(Color.blue)
+                 .cornerRadius(10)
+                 .padding()
+             
+             Button(action: myBT.decreaseNumber) {
+                 Image(systemName: "arrow.down")
+                     .font(.system(size: 24))
+                     .foregroundColor(.white)
+                     .padding()
+                     .frame(height: 48)
+                     .background(Color.red)
+                     .cornerRadius(5)
+             }
+         }
+         .padding()
+     }
+
+     private var bottomButtons: some View {
+         HStack {
+             Spacer()
+             Button(action: startScanning) {
+                 Image(systemName: "play.circle")
+                     .resizable()
+                     .frame(width: 50, height: 50)
+             }
+             
+             Spacer()
+             
+             Button(action: stopScanning) {
+                 Image(systemName: "stop.circle")
+                     .resizable()
+                     .frame(width: 50, height: 50)
+                     .foregroundColor(.red)
+             }
+             Spacer()
+         }
+         .padding()
+     }
+
+     private func startScanning() {
+         myBT.startScanning()
+     }
+
+     private func stopScanning() {
+         myBT.stopScanning()
+     }
+}
 
 
 
