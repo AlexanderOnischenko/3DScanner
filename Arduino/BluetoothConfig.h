6bad4ae36d20bf1ed3c2d98327991caf57c04e88
Alex <onischenko_ay@inbox.ru>
2024-04-28 14:15:27 +0300

    Create BluetoothConfig.h

---
+++ Arduino/BluetoothConfig.h
@@ -0,0 +1,17 @@
+#ifndef BluetoothConfig_h
+#define BluetoothConfig_h
+
+// device attributes
+
+#define DEVICE_NAME "BT05"
+#define SERVICE_UUID "FFE0"
+#define CHARACTERISTIC_UUID "FFE1"
+
+// protocol conventions
+#define TERM_CHAR 'b'
+#define ROTATE_CHAR '8'
+#define ROTATE_BACKWARDS_CHAR '9'
+#define SET_NUMSHOTS_START 'S'
+#define SET_NUMSHOTS_END 'X'
+
+#endif
